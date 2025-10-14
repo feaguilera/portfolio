da1b7a9f53cfc8d01caabff53d30fb95cc4702e1
Felipe Aguilera Bertolini <61721430+feaguilera@users.noreply.github.com>
2025-10-13 23:44:21 -0300

    atualizacao

---
+++ nivel-avancado/projeto/primeflix/src/App.js
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <div className="App">
+      <h1>PrimeFlix</h1>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ nivel-avancado/projeto/primeflix/src/pages/filme/index.js
@@ -0,0 +1,5 @@
+function filme() {
+    return <h1>Bem vindo a pagina de Filmes</h1>;
+    }
+    
+export default filme;
